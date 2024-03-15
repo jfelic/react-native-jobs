ad4e0258c99a54d5389fbeda970828024a32ec60
Julian Feliciano <jfelic@Julians-MacBook-Pro.local>
2024-03-14 22:23:41 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { Link, Stack } from "expo-router";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
